ab7a0a546729b6d9dec70e5e93230e1246ac6548
kimbielby <kib00075@students.stir.ac.uk>
2021-12-02 18:07:55 +0000

    Version 1 of RingNode constructor.  Added/altered Socket variable names.

--- src/RingNode.java
+++ src/RingNode.java
@@ -1,10 +1,61 @@
 import java.net.*;
 
 public class RingNode {
-    private ServerSocket s;
+    private ServerSocket servSock;
+    private Socket thisNodeSock;
+    private Socket nextNodeSock;
 
-    public RingNode(int this_port, String next_host, int next_port){
-        
+    public RingNode(int thisPort, String nextHost, int nextPort){
+
+        // Create the server socket with the current port
+        try {
+            servSock = new ServerSocket(thisPort);
+            System.out.println("Server socket on ring node is listening ....");
+        }
+        catch (Exception e){}
+
+        // Listen for connections and create them
+        while (true) {
+            try {
+                // Accept the server socket and the token
+                thisNodeSock = servSock.accept();
+
+                // Confirm that token has been received
+                System.out.println("Server socket has received the token");
+
+                // DO SECTION TO UPDATE THE FILE
+
+                // Close the current socket
+                thisNodeSock.close();
+
+                // Create a new socket to send the token to
+                nextNodeSock = new Socket(nextHost, nextPort);
+
+                // Check that the connection was successful
+                if (nextNodeSock.isConnected()){
+                    // Do a print line
+                }
+                else{
+                    // Do a print line
+                }
+
+                // DO A TRY-CATCH THREAD SLEEP FOR A WEE BIT
+
+                // Close the new socket
+                nextNodeSock.close();
+
+                // DO A TRY-CATCH THREAD SLEEP FOR A WEE BIT
+
+                //Check that the connection was successfully closed
+                if (nextNodeSock.isClosed()){
+                    // Do a print line
+                }
+                else {
+                    // Do a print line
+                }
+            }
+            catch (Exception e){}
+        }
     }
 
     public static void main(String args[]){
